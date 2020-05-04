8dddb23d831365a961c0379dc8c8ccaccaa3584d
Jason <jlong5795@gmail.com>
2020-05-04 16:48:15 -0500

    more progress

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -34,7 +34,13 @@ waypoints = [
 ]
 
 # Add a new waypoint to the list
-# YOUR CODE HERE
+waypoints.append({
+        "lat": 46,
+        "lon": -132,
+        "name": "a fourth place"
+    })
+
+print(waypoints)
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
